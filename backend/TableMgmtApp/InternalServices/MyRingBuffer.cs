7a446efcefab75d7836d9b7a6b2fd8cc8701b17e
Gediminas Blazys <blazys@pm.me>
2025-05-12 23:32:12 +0300

    Removing comments.

--- backend/TableMgmtApp/InternalServices/MyRingBuffer.cs
+++ backend/TableMgmtApp/InternalServices/MyRingBuffer.cs
@@ -76,27 +76,4 @@ public class RingBuffer<T> {
     }
 }
 
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue obj = new MyCircularQueue(k);
- * bool param_1 = obj.EnQueue(value);
- * bool param_2 = obj.DeQueue();
- * int param_3 = obj.Front();
- * int param_4 = obj.Rear();
- * bool param_5 = obj.IsEmpty();
- * bool param_6 = obj.IsFull();
- */
-
-/**
-  MyCircularQueue(k) Initializes the object with the size of the queue to be k.
-  int Front() Gets the front item from the queue. If the queue is empty, return -1.
-  int Rear() Gets the last item from the queue. If the queue is empty, return -1.
-  boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
-  boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
-  boolean isEmpty() Checks whether the circular queue is empty or not.
-  boolean isFull() Checks whether the circular queue is full or not.
-  */
-
-
-
 
